b675dbc8c2e81d961fd249624e37008d7a9ee02c
William Lyon <lyonwj@gmail.com>
2012-02-06 15:18:41 -0700

    Initial Commit

---
+++ CoreLocationTest/CLAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CLAppDelegate.h
+//  CoreLocationTest
+//
+//  Created by William Lyon on 2/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CLViewController;
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CLViewController *viewController;
+
+@end
---
+++ CoreLocationTest/CLViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLViewController.h
+//  CoreLocationTest
+//
+//  Created by William Lyon on 2/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLViewController : UIViewController
+
+@end
